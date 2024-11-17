0b89c675b59c1e762232385c4d765534332bd36e
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-11-17 12:34:04 +0530

    Hello I am making new project for practicing OOPS concept.

---
+++ src/T1Class/Main.java
@@ -0,0 +1,8 @@
+package T1Class;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello I am starting OOPS Concept Revision");
+
+    }
+}
--- src/T1Class/Main1.java
+++ src/T1Class/Main1.java
@@ -1,4 +1,6 @@
-public class Main {
+package T1Class;
+
+public class Main1 {
     public static void main(String[] args) {
         System.out.println("Hello I am starting OOPS Concept Revision");
 
