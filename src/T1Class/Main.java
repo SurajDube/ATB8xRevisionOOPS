1c28b719b0d43bcd38d043f8b6b1d567f50bc12b
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-11-17 18:38:55 +0530

    Repractice class object default constructor, types of variable.

--- src/T1Class/Main.java
+++
@@ -1,8 +0,0 @@
-package T1Class;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello I am starting OOPS Concept Revision");
-
-    }
-}
---
+++ src/Topic1ClassObject/object.java
@@ -0,0 +1,18 @@
+package Topic1ClassObject;
+
+public class object {
+    int x = 5;
+    public static void m1(){   // static method can call directly
+        System.out.println("this is a static method.");
+    }
+    public void m2(){
+        System.out.println("this is a non static method.");
+    }
+    public static void main(String[] args) {
+        m1();
+        // how to call non static method
+        // using creating object
+        object o = new object();
+        o.m2();
+    }
+}
